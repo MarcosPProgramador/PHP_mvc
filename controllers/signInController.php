6d01d67d3a34d01569e55556f26d32a36afdb15b
x14102004x <marcosproenca144@gmail.com>
2020-12-05 20:39:59 -0300

    Added signUp

--- controllers/signInController.php
+++ controllers/signInController.php
@@ -14,6 +14,7 @@ class signInController {
             $this->datas = $datas;
             $this->allowAccess();
             
+            $this->test();
         }
     }
     public function test(){
